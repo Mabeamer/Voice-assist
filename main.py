fad2a63caab4bebe39bccb6483c9fd12debc3291
Alex <Mabeamer01@gmail.com>
2024-03-02 14:32:26 -0500

    test

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-import gpt import userDirection
+from gpt import userDirection
+import keyboard
 
+print("Listening for recording....")
+
+
+while True:
+    try:
+        if keyboard.is_pressed('v'):
+            print("holding v")
+            break
+    except:
+        break
